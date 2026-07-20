0cb2448271391da9e8ce3aeac0c69366197d2739
Aringgrh <Aaahary14@gmail.com>
2026-07-20 12:03:01 +0700

    Tugas 3 loop

---
+++ Tugas3.dart
@@ -0,0 +1,58 @@
+void tampilBilGanjil () {
+  for (int i = 0; i <= 20; i++) {
+    if ( i % 2 != 0) {
+      print ("Bilangan Ganjil $i");
+    }
+  }
+}
+
+void cetakKarakter (){
+int i = 1;
+
+while (i<=5){
+  print("*");
+  i++;
+}
+}
+
+void namaBerulang (){
+
+  String nama = "Ari";
+  int i = 1;
+ while (i<5){
+        print(nama);
+ i++;
+ }
+ 
+}
+
+void perulanganList(){
+  List<String> buah = ["Apel", "Jeruk", "Mangga", "Amggur"];
+
+  for ( var b in buah){
+       print("Saya Suka Buah $b");
+  }
+}
+
+void simulasiBelanja(){
+
+  List<String> belanjaan = ["Beras", "Minyak", "telur", "Kecap", "Saos"];
+
+  for (var bn in belanjaan) {
+      print("Item Ke-${belanjaan.indexOf(bn)+1}: $bn");
+  }
+}
+
+
+
+void main(){
+
+  tampilBilGanjil();
+  cetakKarakter();
+  namaBerulang();
+  perulanganList();
+  simulasiBelanja();
+}
+
+
+
